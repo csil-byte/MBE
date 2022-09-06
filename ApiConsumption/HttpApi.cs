237a09537c62055e55f0538788790b871a295850
csil-byte <cgscsilva@gmail.com>
2022-09-06 00:36:01 -0300

    api consumption - done

---
+++ ApiConsumption/HttpApi.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+
+namespace ApiConsumption
+{
+    public static class HttpApi
+    {
+        private static HttpClient client = null;
+
+        public static string GetApi(string path)
+        {
+            if (client == null)
+            {
+                HttpClientHandler handler = new HttpClientHandler()
+                {
+                    AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate
+                };
+                client = new HttpClient(handler);
+            }
+            client.BaseAddress = new Uri(path);
+            HttpResponseMessage response = client.GetAsync("answers?order=desc&sort=activity&site=stackoverflow").Result;
+            response.EnsureSuccessStatusCode();
+            string result = response.Content.ReadAsStringAsync().Result;
+            Console.WriteLine("Result: " + result);
+
+            return result;
+
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,45 +1,50 @@
 ﻿using System;
 using MySql.Data.MySqlClient;
 using System.Diagnostics;
-using System.Collections.Generic;
-using System.Data;
-using System.Data.SqlClient;
-
-
-var connString = "Server=35.175.112.147;Port=3306;Uid=testuser;Password=Test.344;Database=test";
-using (var conn = new MySqlConnection(connString))
-{
-    await conn.OpenAsync();
-
-    using (var cmd = new MySqlCommand("SELECT * FROM schedule", conn))
-    using (var reader = await cmd.ExecuteReaderAsync())
-        while (await reader.ReadAsync())
-            // A) LECTURA DE LA TABLA 
-            do
-            {
-                Console.WriteLine(reader.GetString(2));
-
-                if (reader.GetString(1) == "API")
-                {
-                    Console.WriteLine("API");
-                }
-                if (reader.GetString(1) == "LINUX")
-                {
-                    Process.Start("C://acrobat.exe");
-                }
-
-            } while (reader.Read());
-    Console.ReadLine();
-}
-
+using ApiConsumption;
 
 namespace MBE_PRUEBA_22
 {
     internal class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+        {             
+           try
+            {
+                using var connection = new MySqlConnection("Server=35.175.112.147;Port=3306;Uid=testuser;Password=Test.344;Database=test");
+                connection.Open();
+
+                using var command = new MySqlCommand("SELECT * FROM schedule;", connection);
+                using var reader = command.ExecuteReader();
+                while (reader.Read())
+                  
+                    // A) LECTURA DE LA TABLA 
+                    do
+                    {
+                        if (reader.GetString(1) == "API")
+                        {
+                            HttpApi.GetApi(reader.GetString(2));
+
+                        }
+                    //    if (reader.GetString(1) == "LINUX")
+                    //    {
+                    //    Console.WriteLine("if LIN");
+                    //}
+
+                    } while (reader.Read());
+                Console.ReadLine();
+
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+               
+
+          
         }
+
     }
 }
+
+
